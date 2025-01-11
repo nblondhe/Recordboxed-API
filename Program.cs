5dd4eca10c592e9eb00e00d6f5ffd5d1ad2fffb4
njb <njblondhe@gmail.com>
2025-01-10 19:29:32 -0600

    Add prod conn string

--- Program.cs
+++ Program.cs
@@ -16,16 +16,16 @@ builder.Services.AddSwaggerGen();
 var dbHost = builder.Configuration["RB:HOST"];
 var serverName = builder.Configuration["RB:SERVERNAME"];
 var dbName = builder.Configuration["RB:DBNAME"];
-var productionConnectionString = builder.Configuration["AZ:ConnectionString:Recordboxed:SQLDB"];
-// builder.Services.AddSqlServer<DataContext>(productionConnectionString, options => options.EnableRetryOnFailure());
+var productionConnectionString = builder.Configuration["ConnectionStrings:Recordboxed:AZSQLDB"];
+builder.Services.AddSqlServer<DataContext>(productionConnectionString, options => options.EnableRetryOnFailure());
 
-builder.Services.AddDbContext<DataContext>(options => {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!
-    .Replace("{RB:HOST}", dbHost)
-    .Replace("{RB:SERVERNAME}", serverName)
-    .Replace("{RB:DBNAME}", dbName);
-    options.UseSqlServer(connectionString);
-});
+// builder.Services.AddDbContext<DataContext>(options => {
+//     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!
+//     .Replace("{RB:HOST}", dbHost)
+//     .Replace("{RB:SERVERNAME}", serverName)
+//     .Replace("{RB:DBNAME}", dbName);
+//     options.UseSqlServer(connectionString);
+// });
 
 var app = builder.Build();
 app.Logger.LogInformation("=== LOGGING STARTED");
